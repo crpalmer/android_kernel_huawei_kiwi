3e924ca647add854397c68595c3f4b0d47bcd676
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-28 05:12:49 -0400

    kiwi: cpufreq: Rename conflicting variable name in qcom-cpufreq
    Change-Id: I882ff19158f2c0cede5d23747c3bf6d4fd1d5854

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -45,7 +45,7 @@ struct cpufreq_suspend_t {
 
 static unsigned int cpu0_max_freq = 0;
 
-static DEFINE_PER_CPU(struct cpufreq_suspend_t, cpufreq_suspend);
+static DEFINE_PER_CPU(struct cpufreq_suspend_t, cpufreq_suspend_data);
 
 static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 			unsigned int index)
@@ -100,12 +100,12 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	int index;
 	struct cpufreq_frequency_table *table;
 
-	mutex_lock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
+	mutex_lock(&per_cpu(cpufreq_suspend_data, policy->cpu).suspend_mutex);
 
 	if (target_freq == policy->cur)
 		goto done;
 
-	if (per_cpu(cpufreq_suspend, policy->cpu).device_suspended) {
+	if (per_cpu(cpufreq_suspend_data, policy->cpu).device_suspended) {
 		pr_debug("cpufreq: cpu%d scheduling frequency change "
 				"in suspend.\n", policy->cpu);
 		ret = -EFAULT;
@@ -127,7 +127,7 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	ret = set_cpu_freq(policy, table[index].frequency,
 			   table[index].driver_data);
 done:
-	mutex_unlock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
+	mutex_unlock(&per_cpu(cpufreq_suspend_data, policy->cpu).suspend_mutex);
 	return ret;
 }
 
@@ -262,9 +262,9 @@ static int msm_cpufreq_suspend(void)
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
-		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
-		per_cpu(cpufreq_suspend, cpu).device_suspended = 1;
-		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+		mutex_lock(&per_cpu(cpufreq_suspend_data, cpu).suspend_mutex);
+		per_cpu(cpufreq_suspend_data, cpu).device_suspended = 1;
+		mutex_unlock(&per_cpu(cpufreq_suspend_data, cpu).suspend_mutex);
 	}
 
 	return NOTIFY_DONE;
@@ -276,7 +276,7 @@ static int msm_cpufreq_resume(void)
 	struct cpufreq_policy policy;
 
 	for_each_possible_cpu(cpu) {
-		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
+		per_cpu(cpufreq_suspend_data, cpu).device_suspended = 0;
 	}
 
 	/*
@@ -514,8 +514,8 @@ static int __init msm_cpufreq_register(void)
 	int cpu, rc;
 
 	for_each_possible_cpu(cpu) {
-		mutex_init(&(per_cpu(cpufreq_suspend, cpu).suspend_mutex));
-		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
+		mutex_init(&(per_cpu(cpufreq_suspend_data, cpu).suspend_mutex));
+		per_cpu(cpufreq_suspend_data, cpu).device_suspended = 0;
 	}
 
 	rc = platform_driver_probe(&msm_cpufreq_plat_driver,
@@ -524,7 +524,7 @@ static int __init msm_cpufreq_register(void)
 		/* Unblock hotplug if msm-cpufreq probe fails */
 		unregister_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
 		for_each_possible_cpu(cpu)
-			mutex_destroy(&(per_cpu(cpufreq_suspend, cpu).
+			mutex_destroy(&(per_cpu(cpufreq_suspend_data, cpu).
 					suspend_mutex));
 		return rc;
 	}
